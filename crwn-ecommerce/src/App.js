14deb9b6917f5f9c76d8af4aaf61a99e3565eb6f
hgmancholam <hgmancholam@gmail.com>
2023-07-21 09:44:15 -0500

    adding scss

--- crwn-ecommerce/src/App.js
+++ crwn-ecommerce/src/App.js
@@ -1,5 +1,43 @@
+import "./categories.style.scss";
+
+
 const App = () => {
-  return <div>Hello world</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Women",
+    },
+    {
+      id: 4,
+      title: "Men",
+    },
+    {
+      id: 5,
+      title: "Children",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div className="category-container">
+          <div className="background-img"></div>
+          <div className="category-body-containter" key={id}>
+            <h2>{title}</h2>
+            <p>shop now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
