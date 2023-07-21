b8a709351639d83908fce4f9d36c6a69c9fffb3a
hgmancholam <hgmancholam@gmail.com>
2023-07-21 12:33:55 -0500

    Adding navigarion bar

---
+++ crwn-ecommerce/src/components/shop/shop.component.jsx
@@ -0,0 +1,6 @@
+import "./shop.styles.scss";
+
+const Shop = () => {
+  return <h1>im on shop file</h1>;
+};
+export default Shop;
---
+++ crwn-ecommerce/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+import "./navigation.styles.scss";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/home">
+            HOME
+          </Link>
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Navigation;
