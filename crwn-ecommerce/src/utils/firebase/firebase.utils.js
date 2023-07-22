f51b4fa98b78cb329b2e766ede130883be89d06c
hgmancholam <hgmancholam@gmail.com>
2023-07-21 20:32:30 -0500

    adding firebase

---
+++ crwn-ecommerce/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAt4DXcnw-1p-4QDE_mUz3NmXhha739U9w",
+  authDomain: "crwn-ecommerce-project.firebaseapp.com",
+  projectId: "crwn-ecommerce-project",
+  storageBucket: "crwn-ecommerce-project.appspot.com",
+  messagingSenderId: "667610961800",
+  appId: "1:667610961800:web:00fd46e05903aa32e5cc3d",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
